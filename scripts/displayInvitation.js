28be55f1ab8411c3850e0c072dd410bc732a5b27
YenDuyenLeMavs <yenduyen.le@mavs.uta.edu>
2023-11-25 19:46:52 -0600

    removed "Invitation Date"

--- scripts/displayInvitation.js
+++ scripts/displayInvitation.js
@@ -5,15 +5,12 @@ document.addEventListener("DOMContentLoaded", function() {
     const noBtn = document.querySelector(".no-btn");
     const gif = document.querySelector(".gif");
 
-    // Retrieve custom name, question, and date from URL parameters
     const urlParams = new URLSearchParams(window.location.search);
-    // const name = urlParams.get('name'); // Default to "Friend" if name is not provided
     const question = urlParams.get('question');
     const date = urlParams.get('date');
 
     invitationQuestion.textContent = `${question}`;
 
-    // Handle click on "Yes" button
     yesBtn.addEventListener("click", () => {
         if (date) {
             invitationQuestion.textContent = `Yay, see you on ${date}`;
@@ -21,14 +18,12 @@ document.addEventListener("DOMContentLoaded", function() {
             invitationQuestion.textContent = `Yay, see you soon!`;
         }
         invitationDate.textContent = ''
-        // Change GIF source here if needed
+
         gif.src = "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
         
-        // Remove the "Yes" and "No" buttons
-        yesBtn.parentNode.remove(); // Removes the parent container of "Yes" button (assuming it wraps both buttons)
+        yesBtn.parentNode.remove(); 
     });
 
-    // Handle mouseover on "No" button
     noBtn.addEventListener("mouseover", () => {
         const noBtnRect = noBtn.getBoundingClientRect();
         const maxX = window.innerWidth - noBtnRect.width;
--- scripts/gatherInfo.js
+++ scripts/gatherInfo.js
@@ -1,27 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
     const form = document.getElementById("customForm");
-    // const invitationLink = document.getElementById("invitationLink");
-    // const generateLinkBtn = document.getElementById('generateLinkBtn');
     const copyLinkBtn = document.getElementById('copyLinkBtn');
     const previewBtn = document.getElementById('previewBtn');
-    // const newInvitationLink = document.getElementById('newInvitationLink');
+    
     form.addEventListener("submit", function(event) {
         event.preventDefault();
 
-        // Retrieve input values from the form
-        // const name = document.getElementById("name").value;
         const question = document.getElementById("question").value || "Will you go out with me?";
         const date = document.getElementById("date").value;
 
-        // Generate a custom link with the provided information
-        // const customLink = `https://yenduyenlemavs.github.io/ForcedInvite/invitation.html?name=${encodeURIComponent(name)}&question=${encodeURIComponent(question)}&date=${encodeURIComponent(date)}`;
         const customLink = `https://yenduyenlemavs.github.io/ForcedInvite/invitation.html?question=${encodeURIComponent(question)}&date=${encodeURIComponent(date)}`;
 
-        // Set the generated link as the href attribute of the anchor element
-        // invitationLink.href = customLink;
-        // invitationLink.innerHTML = `<a href="${customLink}" target="_blank">${customLink}</a>`;
-
-        // document.getElementById("link-container").innerHTML = `<a href="${customLink}">${customLink}</a>`;
         document.getElementById("copyLinkBtn").addEventListener("click", function() {
             const copiedLink = customLink;
             const textarea = document.createElement('textarea');
@@ -30,28 +19,13 @@ document.addEventListener("DOMContentLoaded", function() {
             textarea.select();
             document.execCommand('copy');
             document.body.removeChild(textarea);
-            // alert("Link copied to clipboard!");
         });
 
-        // Function to open the invitation in a new window
         document.getElementById("previewBtn").addEventListener("click", function() {
             window.open(customLink);
         });
 
-        // generateLinkBtn.style.display = 'none';
         copyLinkBtn.style.display = 'inline-block';
         previewBtn.style.display = 'inline-block';
-        // newInvitationLink.style.display = 'block';
-        // newInvitationLink.setAttribute('href', '#');
-
     });
-    // newInvitationLink.addEventListener('click', function(event) {
-    //     event.preventDefault();
-
-    //     // Show the "Generate Link" button and hide "Copy Link" and "Preview" buttons
-    //     // generateLinkBtn.style.display = 'block';
-    //     copyLinkBtn.style.display = 'none';
-    //     previewBtn.style.display = 'none';
-    //     newInvitationLink.style.display = 'none';
-    // });
 });
